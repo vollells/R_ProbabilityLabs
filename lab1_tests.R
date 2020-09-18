ab2e49ae7c252819c379e2e4792553d5959b7d06
vollells <vicle728@student.liu.se>
2020-09-18 09:52:24 +0200

    More tasks done

--- lab1_tests.R
+++ lab1_tests.R
@@ -125,14 +125,117 @@ lines(xfit, yfit, col="blue", lwd=2)
 
 #Uppgift 3.1.4
 
-binom <- pbinom(10000, 10, 0.1)
-binom
+binom <- rbinom(10000, 10, 0.1)
+sum(binom==0)
+
+#X<0
+#CDF
+norm <- pnorm(0,0,1, TRUE)
+norm
+
+#Test
+norm <- rnorm(10000,0,1)
+sum(norm<0)/10000
 
 
+#-1<X<1
+#CDF
+normleft <- pnorm(-1,0,1, TRUE)
+normright  <- pnorm(1,0,1, TRUE)
+normright - normleft
 
-xfit <- seq(-1, 10, 1) 
-yfit <- dbinom(xfit, 10, 0.1)
-yfit 
+#Test
+norm <- rnorm(10000,0,1)
+(sum(norm<1)-sum(norm< -1))/10000
 
 
+#1.96 < X
+#CDF
+norm  <- pnorm(1.96,0,1, TRUE)
+norm
+
+#Test
+norm <- rnorm(10000,0,1)
+sum(norm<1.96)/10000
+
+
+#0 < Y < 10 
+#CDF
+binomleft <- pbinom(0,10,0.1) 
+binomright <- pbinom(9,10,0.1)
+binomright - binomleft
+
+#Test
+binom <- rbinom(10000,10,0.1) 
+(sum(binom<10)-sum(binom==0))/10000
+
+#Y=0
+#CDF
+binomleft <- pbinom(0,10,0.1) 
+binomleft
+
+#Test
+binom <- rbinom(10000,10,0.1) 
+sum(binom==0)/10000
+
+
+#0 =< Y =< 10
+#CDF
+binomright <- pbinom(10,10,0.1)
+binomright
+
+#Test
+binom <- rbinom(10000,10,0.1) 
+(sum(binom<=10)-sum(binom<0))/10000
+
+
+#3.1.5
+#1
+#Första Systemet
+binom <- rbinom(10000,337,0.1)
+sum(binom)/10000
+
+#Nya Systemet
+unif <- runif(10000,0.02,0.16)
+binom <- rbinom(10000,337,unif)
+sum(binom)/10000
+
+#2
+#Första Systemet
+binomOld <- rbinom(10000,337,0.1)
+#Nya Systemet
+unif <- runif(10000,0.02,0.16)
+binomNew <- rbinom(10000,337,unif)
+sum(binomOld<binomNew)/10000
+
+#3
+#Första Systemet
+binom <- rbinom(10000,337,0.1)
+sum(binom>50)/10000
+
+#Nya Systemet
+unif <- runif(10000,0.02,0.16)
+binom <- rbinom(10000,337,unif)
+sum(binom>50)/10000
+
+
+#3.2.1
+#1 E(X) = 2 | E(Y) = 1
+#2
+seq <- c()
+for(j in 1:3){
+    for(i in 1:10){
+        seq <- c(seq, i*10^j) 
+    }
+}
+meansX <- c()
+meansY <- c()
+for (n in seq){
+    binom <- rbinom(n,10,0.2)
+    meansX <- c(meansX, mean(binom))
+    gamma <- rgamma(n,2,2)
+    meansY <- c(meansY, mean(gamma))
+}
+plot(seq,meansX,type="l")
+plot(seq,meansY, type="l")
 
